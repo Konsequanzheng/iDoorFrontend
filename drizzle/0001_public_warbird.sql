447f67219f133b4bd92007dc53574833403d839a
Quan Zheng <quan.zheng2@gmail.com>
2026-04-23 17:58:03 +0200

    Update drizzle migration to be up to date

--- drizzle/0001_public_warbird.sql
+++ drizzle/0001_public_warbird.sql
@@ -1,1 +1,5 @@
 ALTER TABLE "bookings" ALTER COLUMN "check_in_time" SET DEFAULT '13:00:00';--> statement-breakpoint
+ALTER TABLE "bookings" ALTER COLUMN "check_in_time" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "bookings" ALTER COLUMN "check_out_time" SET DEFAULT '10:00:00';--> statement-breakpoint
+ALTER TABLE "bookings" ADD COLUMN "guest" text;--> statement-breakpoint
+ALTER TABLE "bookings" ADD COLUMN "room" text;
