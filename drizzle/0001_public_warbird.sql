d63c306055ef23099ebd7076663d7e3407b9450b
Quan Zheng <quan.zheng2@gmail.com>
2026-04-23 17:28:59 +0200

    Pull supabase schema and change default check-in time to 1pm

---
+++ drizzle/0001_public_warbird.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "bookings" ALTER COLUMN "check_in_time" SET DEFAULT '13:00:00';--> statement-breakpoint
--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,9 +1,11 @@
-import { date, pgTable, text, time } from "drizzle-orm/pg-core";
+import { pgTable, text, date, time } from "drizzle-orm/pg-core";
 
 export const bookings = pgTable("bookings", {
-  id: text("id").primaryKey(),
+  id: text("id").primaryKey().notNull(),
   fromDate: date("from_date").notNull(),
   toDate: date("to_date").notNull(),
-  checkInTime: time("check_in_time").default("12:00").notNull(),
-  checkOutTime: time("check_out_time").default("10:00").notNull(),
+  checkInTime: time("check_in_time").default("13:00:00"),
+  checkOutTime: time("check_out_time").default("10:00:00").notNull(),
+  guest: text("guest"),
+  room: text("room"),
 });
